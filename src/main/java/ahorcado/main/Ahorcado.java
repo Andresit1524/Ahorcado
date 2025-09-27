ce3c99d539b58fd1cbca03f49018211b346c5240
Hayran Andrés López <andreslopez01182009@gmail.com>
2025-09-27 13:29:48 -0500

    Commit inicial

---
+++ src/main/java/ahorcado/main/Ahorcado.java
@@ -0,0 +1,14 @@
+package ahorcado.main;
+
+/**
+ *
+ * @author Hayran Andrés López González
+ * @version 1.0
+ */
+
+public class Ahorcado {
+
+    public static void main(String[] args) {
+
+    }
+}
