db58a7f6b4cf31d1a1e886546271d3b630752819
Hayran Andrés López <andreslopez01182009@gmail.com>
2025-10-01 15:40:45 -0500

    Versión final 1.3.1

--- src/main/java/ahorcado/main/Ahorcado.java
+++ src/main/java/ahorcado/main/Ahorcado.java
@@ -5,29 +5,28 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 /**
+ * Implementa la lógica para un juego del ahorcado basado en consola. Incluye un
+ * monigote del ahorcado en ASCII, cantidad de errores máximos y indicador de
+ * letras usadas.
  *
  * @author Hayran Andrés López González
- * @version 1.3
+ * @version 1.3.1
  */
-
 public class Ahorcado {
     Random r = new Random();
     Scanner s = new Scanner(System.in);
 
-    // Atributos estáticos
     private static final int MAX_ERRORS = 7; // Máximo de errores permitidos
-    private static final String HANGMAN_STRING = "q(x_x)p"; // Dibujo ASCII del monigote
+    private static final String HANGMAN_STRING = "q(x_x)p"; // Dibujo ASCII del ahorcado
 
-    // Arreglo de palabras predefinidas para elegir (también estático)
+    // Lista de palabras predefinidas para jugar
     private static final String[] wordsArray = { "herencia", "instancia", "interfaz", "compilar", "mensajes",
             "privadas", "paquetes", "ejecutar", "propiedad", "abstracto" };
 
-    // Atributos de instancia
     private String word; // Palabra a adivinar
-    private ArrayList<Character> usedLetters; // Lista de letras usadas
+    private ArrayList<Character> usedLetters; // Lista de letras que el usuario ya ha intentado
     private int currErrors; // Cantidad de errores actuales
 
-    // Constructor
     public Ahorcado() {
         word = wordsArray[r.nextInt(wordsArray.length)];
         usedLetters = new ArrayList<Character>();
@@ -36,33 +35,33 @@ public class Ahorcado {
 
     /**
      * Dibuja la figura del ahorcado en función del número de errores cometidos y la
-     * cantidad máxima de errores permitidos
+     * cantidad máxima de errores permitidos.
      */
     private String drawHangman(int errorCount) {
-        // Calcula la proporción de la figura a dibujar basándose en los intentos
+        // Calcula la proporción de la figura a dibujar basándose en los errores
         int charsToDraw = (int) ((double) errorCount / Ahorcado.MAX_ERRORS * Ahorcado.HANGMAN_STRING.length());
 
-        // Obtiene la parte de la figura que se debe mostrar
-        String hangmanPart = Ahorcado.HANGMAN_STRING.substring(0, charsToDraw);
-
-        return hangmanPart;
+        return Ahorcado.HANGMAN_STRING.substring(0, charsToDraw);
     }
 
     /**
-     * Dibuja la palabra en función de la lista de letras usadas. La lista también
-     * incluye letras que son elecciones incorrectas. El truco para que funcione
-     * esta función, entonces, está en que esas letras no están en la palabra (por
-     * eso son incorrectas) y por ende pasan derecho sin afectar el resultado
+     * Dibuja la palabra en función de la lista de letras usadas, mostrando barras
+     * bajas donde no se ha adivinado.
+     * 
+     * Sin embargo, la lista de letras usadas también incluye letras que son
+     * elecciones incorrectas. El truco para que funcione esta función, entonces,
+     * está en que esas letras no están en la palabra (por eso son incorrectas) y
+     * por ende son ignoradas sin afectar el resultado.
      */
     private String drawWord() {
         String finalWord = "";
 
-        // Recorre por cada letra de la palabra
         for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
 
-            // Si la letra está en la lista, la añade a la palabra final y se sale del bucle
-            // Si no se encontró la letra, pone una barra baja
+            // Si la letra está en la lista, la añade a la palabra final
+            // Si no, pone una barra baja
+            // También coloca un espacio después de cada letra
             if (usedLetters.contains(letter)) {
                 finalWord += letter + " ";
             } else {
@@ -78,53 +77,52 @@ public class Ahorcado {
 
     /**
      * Verifica que la letra elegida esté en la palabra y no haya sido usada antes.
-     * Res => 1: letra correcta, 0: letra ya evaluada antes, -1: letra incorrecta
+     * 
+     * 1: letra correcta.
+     * 0: letra ya evaluada antes.
+     * -1: letra incorrecta.
      */
     private int checkChar(char letter) {
-        // Verifica que la letra fue usada ya y retorna 0 en ese caso
         if (usedLetters.contains(letter)) {
             return 0;
         }
 
-        // Si no, busca en la palabra a por la letra y retorna 1 si la encuentra
         if (word.contains(String.valueOf(letter))) {
             return 1;
         }
 
-        // Si no la encuentra... -1
         return -1;
     }
 
     /**
-     * Obtiene un y solo un caracter del usuario para el juego, filtrando entradas
-     * no válidas. Las entradas no válidas incluyen números, simbolos o más de un
-     * carácter.
+     * Solicita al usuario que ingrese una letra y valida:
+     * 
+     * 1. Que sea un solo caracter
+     * 2. Que sea una letra
      */
     private char getUserChar() {
         // Repite hasta que el usuario ingrese una única letra válida
         while (true) {
-            System.out.println("Ingresa una letra");
-            String gottenChars = s.next().toLowerCase();
+            System.out.printf("Ingresa una letra\n> ");
+            String userChar = s.next().toLowerCase();
 
-            // Verifica que la entrada no esté vacía y tenga un solo carácter
-            if (gottenChars.length() != 1) {
+            if (userChar.length() != 1) {
                 System.out.println("Escribe un solo caracter\n");
-                continue; // Salta al inicio del bucle
+                continue;
             }
 
-            char userChar = gottenChars.charAt(0);
+            char gotChar = userChar.charAt(0);
 
-            // Verifica que el carácter sea una letra
-            if (Character.isLetter(userChar)) {
-                return userChar; // La entrada es válida, retorna el carácter
-            } else {
-                System.out.println("Entrada no válida. Solo se admiten letras\n");
+            if (Character.isLetter(gotChar)) {
+                return gotChar;
             }
+
+            System.out.println("Entrada no válida. Solo se admiten letras\n");
         }
     }
 
     /**
-     * Devuelve la lista de letras usadas en el orden en el que se usaron
+     * Convierte la lista de letras usadas en una cadena.
      */
     private String usedLettersString() {
         String finalString = "";
@@ -137,32 +135,30 @@ public class Ahorcado {
     }
 
     /**
-     * Juega una única partida de ahorcado
+     * Contiene la lógica para ejecutar una única partida del ahorcado. El bucle se interrumpe cuando el jugador gana o pierde.
+     * 
+     * Nota: el uso de break como forma de salir del 'while (true)' permite mejoras
+     * en el tamaño del código y a la lógica del mismo al gestionar directamente las
+     * condiciones para ganar y perder.
      */
     private void play() {
-        // Repite hasta que ganemos o perdamos (ver los 'break') más abajo
+        // Repite hasta que ganemos o perdamos (ver la nota arriba)
         while (true) {
-            // Dibuja la palabra actual, el ahorcado y las letras usadas
-            System.out.printf("\n%s\t%s\t%s\n", drawWord(),
-                    drawHangman(currErrors), usedLettersString());
+            System.out.printf("\n%s\t%s\t%s\n", drawWord(), drawHangman(currErrors), usedLettersString());
 
-            // Letra elegida por el usuario
+            // Recibe la letra del usuario y la valida
             char letter = getUserChar();
-
-            // Verifica la letra y reacciona dependiendo del caso
             int checkResult = checkChar(letter);
+
             switch (checkResult) {
-                // Letra correcta: añade la letra como usada
-                case 1:
+                case 1: // Letra correcta
                     System.out.println("Letra correcta");
                     usedLetters.add(letter);
                     break;
-                // Letra ya evaluada antes
-                case 0:
+                case 0: // Letra ya evaluada
                     System.out.println("Letra ya evaluada antes");
                     break;
-                // Letra incorrecta
-                case -1:
+                case -1: // Letra incorrecta
                     currErrors++;
                     System.out.printf("Letra incorrecta. Te quedan %d intentos\n", MAX_ERRORS - currErrors);
                     usedLetters.add(letter);
@@ -171,7 +167,7 @@ public class Ahorcado {
 
             // Si llegamos al límite de errores, perdemos
             if (currErrors == MAX_ERRORS) {
-                System.out.printf("\n> %s ¡Haz perdido! la palabra era: %s <\n", HANGMAN_STRING, word);
+                System.out.printf("\n> ¡Haz perdido! la palabra era: %s <\n", word);
                 break;
             }
 
@@ -184,7 +180,7 @@ public class Ahorcado {
     }
 
     /**
-     * Resetea los valores del juego a su estado original para empezar otra vez
+     * Reinicia el estado del juego para una nueva partida.
      */
     private void reset() {
         usedLetters.clear();
@@ -193,25 +189,24 @@ public class Ahorcado {
     }
 
     /**
-     * Método principal que gestiona el juego
+     * Inicia y gestiona el flujo principal del juego, permitiendo jugar múltiples
+     * partidas a disposición del usuario.
      */
     public void start() {
-        // Bandera que indica si se repite el juego
-        boolean repeat = true;
-
         System.out.println("--------------------------------------");
         System.out.println("> ¡Bienvenido al juego del ahorcado! <");
         System.out.println("--------------------------------------");
 
-        // Repite el juego mientras queramos repetir
+        boolean repeat = true;
+
+        // Repite el juego mientras lo indique la bandera
         while (repeat) {
             play();
 
-            // Le pedimos al jugador si quiere repetir
+            // Exige al jugador una respuesta entre si o no (s/n)
             System.out.println("\n¿Quieres jugar de nuevo? (s/n)");
             char choice = ' ';
 
-            // Exige al jugador una respuesta entre si o no (s/n)
             while (choice != 's' && choice != 'n') {
                 choice = s.next().toLowerCase().charAt(0);
 
